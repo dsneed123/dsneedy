7f3b7360890fbe15ed659a44b2122895a5dd0824
dsneed123 <101902819+dsneed123@users.noreply.github.com>
2025-01-05 18:05:35 -0800

    invert navbar colors

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -9,61 +9,61 @@ const NavBar = () => {
   return (
     <nav className="p-4 bg-[#1E2A38]">
       <ul className="flex space-x-4">
-        <li>
-          <a
-            href="#about"
-            className="group flex items-center bg-transparent hover:bg-[#00A9E0] text-white rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
-          >
-            <InfoIcon className="text-white group-hover:scale-125 transition-transform duration-300" />
-            <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
-              About
-            </span>
-          </a>
-        </li>
-        <li>
-                <a
-                href="#programming-languages"
-                className="group flex items-center bg-transparent hover:bg-[#00A9E0] text-white rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-60 px-2 py-2"
-                >
-                <CodeIcon className="text-white group-hover:scale-200 transition-transform duration-300" />
-                <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro w-full">
-                  Programming Languages
-                </span>
-                </a>
-        </li>
-        <li>
-          <a
-            href="#Experience"
-            className="group flex items-center bg-transparent hover:bg-[#00A9E0] text-white rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
-          >
-            <WorkOutlineIcon className="text-white group-hover:scale-125 transition-transform duration-300" />
-            <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
-              Experience
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#projects"
-            className="group flex items-center bg-transparent hover:bg-[#00A9E0] text-white rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
-          >
-            <FolderSpecialIcon className="text-white group-hover:scale-125 transition-transform duration-300" />
-            <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
-              Projects
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#contact"
-            className="group flex items-center bg-transparent hover:bg-[#00A9E0] text-white rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
-          >
-            <EmojiPeopleIcon className="text-white group-hover:scale-125 transition-transform duration-300" />
-            <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
-              Contact
-            </span>
-          </a>
-        </li>
+      <li>
+        <a
+        href="#about"
+        className="group flex items-center bg-transparent hover:bg-white text-white hover:text-[#1E2A38] rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
+        >
+        <InfoIcon className="text-white group-hover:text-[#1E2A38] group-hover:scale-125 transition-transform duration-300" />
+        <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
+          About
+        </span>
+        </a>
+      </li>
+      <li>
+        <a
+        href="#programming-languages"
+        className="group flex items-center bg-transparent hover:bg-white text-white hover:text-[#1E2A38] rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-60 px-2 py-2"
+        >
+        <CodeIcon className="text-white group-hover:text-[#1E2A38] group-hover:scale-200 transition-transform duration-300" />
+        <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro w-full">
+          Programming Languages
+        </span>
+        </a>
+      </li>
+      <li>
+        <a
+        href="#Experience"
+        className="group flex items-center bg-transparent hover:bg-white text-white hover:text-[#1E2A38] rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
+        >
+        <WorkOutlineIcon className="text-white group-hover:text-[#1E2A38] group-hover:scale-125 transition-transform duration-300" />
+        <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
+          Experience
+        </span>
+        </a>
+      </li>
+      <li>
+        <a
+        href="#projects"
+        className="group flex items-center bg-transparent hover:bg-white text-white hover:text-[#1E2A38] rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
+        >
+        <FolderSpecialIcon className="text-white group-hover:text-[#1E2A38] group-hover:scale-125 transition-transform duration-300" />
+        <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
+          Projects
+        </span>
+        </a>
+      </li>
+      <li>
+        <a
+        href="#contact"
+        className="group flex items-center bg-transparent hover:bg-white text-white hover:text-[#1E2A38] rounded-full transition-all duration-300 overflow-hidden w-10 hover:w-40 px-2 py-2"
+        >
+        <EmojiPeopleIcon className="text-white group-hover:text-[#1E2A38] group-hover:scale-125 transition-transform duration-300" />
+        <span className="ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap font-source-sans-pro">
+          Contact
+        </span>
+        </a>
+      </li>
       </ul>
     </nav>
   );
